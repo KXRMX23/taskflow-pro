453232d0fa2e7f9071f2549e3df06430279288cf
K V Ritvick Sai <kritvick99@gmail.com>
2026-06-27 01:51:58 +0530

    Phase 8 - Frontend Login Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+import API from "../services/api";
+
 function Dashboard() {
-    return <h1>Dashboard Page</h1>;
+
+  useEffect(() => {
+
+    const testAPI = async () => {
+      try {
+        const res = await API.get("/tasks");
+        console.log(res.data);
+      } catch (err) {
+        console.log(err.response?.data || err.message);
+      }
+    };
+
+    testAPI();
+
+  }, []);
+
+  return (
+    <h1>Dashboard Page</h1>
+  );
 }
 
 export default Dashboard;
