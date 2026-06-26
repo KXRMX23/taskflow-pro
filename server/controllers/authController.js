a775e993eba8883ffc584341528d1a6e253e09e9
K V Ritvick Sai <kritvick99@gmail.com>
2026-06-26 16:51:22 +0530

    Phase 4 - JWT authentication Complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../config/db");
 
 const register = async (req, res) => {
@@ -44,6 +45,67 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if fields are empty
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please fill all fields",
+      });
+    }
+
+    // Find user
+    const user = await db.query(
+      "SELECT * FROM users WHERE email=$1",
+      [email]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].password
+    );
+
+    if (!validPassword) {
+      return res.status(400).json({
+        message: "Invalid Password",
+      });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      {
+        id: user.rows[0].id,
+        email: user.rows[0].email,
+      },
+      "taskflowsecret",
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login Successful",
+      token,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
