84a50dd31b7a8b25638884a64cb38ed21705baec
K V Ritvick Sai <kritvick99@gmail.com>
2026-06-26 18:30:53 +0530

    Phase 5 - Complete Task CRUD API

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -50,7 +50,74 @@ const getTasks = async (req, res) => {
   }
 };
 
+// Update Task
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    const updatedTask = await db.query(
+      `UPDATE tasks
+       SET title=$1,
+           description=$2,
+           status=$3
+       WHERE id=$4
+       RETURNING *`,
+      [title, description, status, id]
+    );
+
+    if (updatedTask.rows.length === 0) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Task Updated Successfully",
+      task: updatedTask.rows[0],
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+// Delete Task
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedTask = await db.query(
+      "DELETE FROM tasks WHERE id=$1 RETURNING *",
+      [id]
+    );
+
+    if (deletedTask.rows.length === 0) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Task Deleted Successfully",
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
 module.exports = {
   createTask,
   getTasks,
+  updateTask,
+  deleteTask,
 };
