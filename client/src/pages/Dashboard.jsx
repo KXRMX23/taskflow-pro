d2d1ace280556e00d12a5bdb5b242b1658b06c28
K V Ritvick Sai <kritvick99@gmail.com>
2026-06-28 00:51:55 +0530

    Phase 9 - React Dashboard CRUD

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import API from "../services/api";
 
 function Dashboard() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+  });
 
   useEffect(() => {
 
     const testAPI = async () => {
       try {
         const res = await API.get("/tasks");
-        console.log(res.data);
+        setTasks(res.data);
       } catch (err) {
         console.log(err.response?.data || err.message);
       }
@@ -17,10 +22,100 @@ function Dashboard() {
     testAPI();
 
   }, []);
+ 
+    const handleChange = (e) => {
+    setNewTask({
+      ...newTask,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await API.post("/tasks", newTask);
+
+        setTasks([...tasks, response.data.task]);
+
+        setNewTask({
+            title: "",
+            description: "",
+        });
+
+    } catch (err) {
+        console.log(err.response?.data || err.message);
+    }
+};   
+
+   const deleteTask = async (id) => {
+  try {
+    await API.delete(`/tasks/${id}`);
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  } catch (err) {
+    console.log(err.response?.data || err.message);
+  }
+};  
 
   return (
-    <h1>Dashboard Page</h1>
+    <div>
+      <h1>Dashboard</h1>
+
+      <form onSubmit={handleSubmit}>
+    <input
+        type="text"
+        name="title"
+        placeholder="Task Title"
+        value={newTask.title}
+        onChange={handleChange}
+    />
+
+    <br /><br />
+
+    <textarea
+        name="description"
+        placeholder="Task Description"
+        value={newTask.description}
+        onChange={handleChange}
+    />
+
+    <br /><br />
+
+    <button type="submit">
+        Create Task
+    </button>
+
+    <hr />
+</form>
+
+     <div>
+    {tasks.map((task) => (
+        <div
+            key={task.id}
+            style={{
+                border: "1px solid gray",
+                padding: "15px",
+                marginBottom: "15px",
+                borderRadius: "8px",
+            }}
+        >
+            <h3>{task.title}</h3>
+
+            <p>{task.description}</p>
+
+            <strong>Status:</strong> {task.status}
+
+            <button onClick={() => deleteTask(task.id)}>
+                Delete Task
+            </button>
+        </div>
+        
+    ))}
+</div> 
+    </div>
   );
 }
+
 
 export default Dashboard;
