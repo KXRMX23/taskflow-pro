72dfd905ef1f7f4c32dd12d0aa47c036f999b09c
K V Ritvick Sai <kritvick99@gmail.com>
2026-07-21 16:18:45 +0530

    Fix Logout redirect to login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Toaster position="top-right" />
 
     <Routes>
+      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/email-verified" element={<EmailVerified />} />
